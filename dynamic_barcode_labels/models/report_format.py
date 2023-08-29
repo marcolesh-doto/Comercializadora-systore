23dabe1de183498002a5a5edf5b9bdef4c7000f0
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2023-08-29 11:50:21 -0600

    Add files via upload

---
+++ dynamic_barcode_labels/models/report_format.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-Present TidyWay Software Solution. (<https://tidyway.in/>)
+
+from odoo import models, fields
+
+
+class report_paperformat(models.Model):
+    _inherit = "report.paperformat"
+    custom_report = fields.Boolean('Temp Formats', default=False)
