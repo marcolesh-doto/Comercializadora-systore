4461d83c21c4dee67f363863621c2b800871d2f0
JoshuaVC6 <129898024+JoshuaVC6@users.noreply.github.com>
2024-05-30 16:52:25 -0600

    webHook

---
+++ webhook_order_confirm/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Order Hook',
+    'version': '1.0',
+    'summary': 'Hook confirmacion orden de venta',
+    'description': 'Lanzara un trigger cuando la orden pase a confirmado',
+    'author': 'Joshua Villela',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ webhook_order_confirm/models/sale_order.py
@@ -0,0 +1,31 @@
+# models/sale_order.py
+from odoo import models, api
+import requests
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for order in self:
+            self._trigger_endpoint(order)
+        return res
+
+    def _trigger_endpoint(self, order):
+        endpoint_url = 'https://odoo.doto.com.mx/api/v1/vtex/invoice/order'
+        headers = {
+            'Content-Type': 'application/json',
+            'mkp': 'dotomxqa'
+        }
+        data = {
+            'order_id': order.id,
+        }
+        response = requests.post(endpoint_url, json=data, headers=headers)
+        if response.status_code != 200:
+            _logger.error(
+                'Failed to trigger endpoint for sale order %s', order.name)
