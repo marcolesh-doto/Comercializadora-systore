47d284beed9b6eb109a0667b61910bc9bc13ae06
JoshuaVC6 <129898024+JoshuaVC6@users.noreply.github.com>
2024-06-05 18:08:04 -0600

    hook

---
+++ webhook_order_confirm/models/stock_picking.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def write(self, vals):
+        res = super(StockPicking, self).write(vals)
+        if 'state' in vals and vals['state'] == 'assigned':
+            for picking in self:
+                if picking.state == 'assigned':
+                    sale_order = picking.sale_id
+                    if sale_order:
+                        sale_order._trigger_endpoint(sale_order)
+        return res
