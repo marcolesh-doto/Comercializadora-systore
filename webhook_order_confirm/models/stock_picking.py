71598df829e1e85ee08d0d93ee2e73898e51346c
JoshuaVC6 <129898024+JoshuaVC6@users.noreply.github.com>
2024-06-07 13:24:26 -0600

    pruebaS

--- webhook_order_confirm/models/stock_picking.py
+++ webhook_order_confirm/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import models, api
+from odoo import models, fields, api
 import requests
 import logging
 
@@ -6,7 +6,8 @@ _logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
+    
+    sale_id = fields.Many2one('sale.order', string='Sale Order', readonly=True)
     def write(self, vals):
         res = super(StockPicking, self).write(vals)
         if 'state' in vals and vals['state'] == 'assigned':
@@ -20,13 +21,19 @@ class StockPicking(models.Model):
             'Content-Type': 'application/json',
             'mkp': 'doto'
         }
+
         sale_order = picking.sale_id
-        data = {
-            'picking_id': picking.id,
-            'state': state,
-            'order_id': sale_order.id if sale_order else None,
-        }
-        response = requests.post(endpoint_url, json=data, headers=headers)
-        if response.status_code != 200:
-            _logger.error(
-                'Failed to trigger endpoint for stock picking %s', picking.name)
+        if sale_order:
+            data = {
+                'picking_id': picking.id,
+                'state': state,
+                'order_id': sale_order.id
+            }
+
+            response = requests.post(endpoint_url, json=data, headers=headers)
+            if response.status_code == 200:
+                _logger.info('Successfully triggered endpoint for stock picking %s', picking.name)
+            else:
+                _logger.error('Failed to trigger endpoint for stock picking %s, response: %s', picking.name, response.text)
+        else:
+            _logger.error('No sale order found for stock picking %s', picking.name)
